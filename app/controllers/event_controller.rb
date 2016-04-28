c8b9815bdb82d4b20ea1ba5ac91590b390a54f2f
Davit Yeghshatyan <6davidy@gmail.com>
2016-04-28 18:56:16 -0400

    Create Event table, seed Database, new Event, view Event, Rake 10.4.2

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,3 +1,14 @@
 class EventController < ApplicationController
+  layout false
 
+  def new
+
+  end
+
+  # noinspection Rails3Deprecated
+  def view
+    @event_id = params['id']
+    @event = Event.find(@event_id)
+    @event_location = @event.location
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create([{location: 'toronto'},{location: 'ottawa'}])
