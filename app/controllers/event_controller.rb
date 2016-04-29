ac5ad11ccecd49c60c804fd1356b9a87ae8d7026
Davit Yeghshatyan <6davidy@gmail.com>
2016-04-28 22:43:45 -0400

    New Event button redirects

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,14 +1,24 @@
 class EventController < ApplicationController
   layout false
 
+  def index
+    redirect_to(:action => 'new')
+  end
+
   def new
 
   end
 
   # noinspection Rails3Deprecated
   def view
-    @event_id = params['id']
-    @event = Event.find(@event_id)
-    @event_location = @event.location
+    puts 'find me'
+    if params[:id] == ''
+      puts params[:id]
+      redirect_to(:action => 'new')
+    else
+      @event_id = params['id']
+      @event = Event.find(@event_id)
+      @event_location = @event.location
+    end
   end
 end
